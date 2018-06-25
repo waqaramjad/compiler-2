38a6cd98c4cba0e9a22ca02fd6a1ac9e5ae5f9cc
waqaramjad <waqaramjad345@gmail.com>
2018-06-25 23:19:46 +0500

    2nd commit

---
+++ compiler/main.js
@@ -0,0 +1,33 @@
+
+// var testInput = 'num oiu;'
+
+// let splitted = testInput.split(" ")
+// // var a = splitted.remove(0)
+// // splitted.splice(0, 1)
+
+// console.log(splitted)
+
+// switch(splitted[0]){
+
+//     case 'num':{
+
+//     }
+
+//     case 'word':{
+
+//     }
+
+// }
+
+class a {
+   
+constructor(){
+
+    // this.b()
+}
+     b() {
+        console.log('hello')
+    }
+}
+const z = new a();
+z.b();
